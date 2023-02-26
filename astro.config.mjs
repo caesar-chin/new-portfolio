fe78dc06cc1d15ac0b1d4517b0643295b4da7d01
caesar-chin <caesarc.1628@gmail.com>
2023-02-25 19:16:59 -0500

    changed to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrjs/vercel/static"
 
 // https://astro.build/config
 export default defineConfig({
